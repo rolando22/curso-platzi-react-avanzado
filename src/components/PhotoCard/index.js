cd4be971c823a0a9f091cd9127a31c9dbd13bb2c
rolando22 <rolando2207@gmail.com>
2022-08-18 21:19:03 -0300

    [ADD] import dinámico de Intersection-observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,16 +9,22 @@ export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
     const [show, setShow] = useState(false);
 
     useEffect(() => {
-        const observer = new window.IntersectionObserver((entries) => {
-            const { isIntersecting } = entries[0];
-            if (isIntersecting) {
-                setShow(true);
-                observer.disconnect();
-            }
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then(() => {
+            const observer = new window.IntersectionObserver((entries) => {
+                const { isIntersecting } = entries[0];
+                if (isIntersecting) {
+                    setShow(true);
+                    observer.disconnect();
+                }
+            });
+            observer.observe(element.current);
         });
-        observer.observe(element.current);
 
-        return () => { observer.disconnect(); }
+        return () => { observer.unobserve(); }
     }, [element]);
 
     return (
