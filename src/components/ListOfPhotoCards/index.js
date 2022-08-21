e7702a4a2a631cdc381116c590c1cdd5b00c8759
rolando22 <rolando2207@gmail.com>
2022-08-20 23:28:03 -0300

    [ADD] GraphQL [UPDATE] ListOfPhotoCards

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 import { PhotoCard } from "../";
 
+const LIST_OF_PHOTO_CARDS = gql`
+    query getPhotos {
+        photos {
+            id
+            categoryId
+            src
+            likes
+            userId
+            liked
+        }
+    }
+`;
+
 export function ListOfPhotoCards () {
+    const { loading, data } = useQuery(LIST_OF_PHOTO_CARDS);
+
     return (
-        <ul>
-            {[1, 2, 3, 4, 5, 6, 7, 8].map(card =>
-                <PhotoCard
-                    key={card}
-                    id={card}
-                />
-            )}
-        </ul>
+        (!loading) && 
+            <ul>
+                {data.photos.map(photo =>
+                    <PhotoCard
+                        key={`photo-${photo.id}`}
+                        id={photo.id}
+                        likes={photo.likes}
+                        src={photo.src}
+                    />
+                )}
+            </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { App } from "./App";
 
-createRoot(document.getElementById('app')).render(<App />);
+const client = new ApolloClient({
+    uri: 'https://petgram-api-2022.vercel.app/graphql',
+    cache: new InMemoryCache()
+});
+
+createRoot(document.getElementById('app'))
+    .render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    );
