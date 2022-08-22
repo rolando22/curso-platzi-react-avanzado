199208c281b053f2ecd74f481d2d023eba5766b3
rolando22 <rolando2207@gmail.com>
2022-08-22 15:04:54 -0300

    [ADD] FavButton y useMutation a PhotoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useMutation, gql } from "@apollo/client";
+import { FavButton } from "../";
 import { useLazyLoad } from "../../hooks/useLazyLoad";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
-import { Articule, ImgWrapper, Img, Button } from "./styles";
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { Articule, ImgWrapper, Img } from "./styles";
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+const LIKE_PHOTO = gql`
+    mutation likeAnonymousPhoto($input: LikePhoto!) {
+      likeAnonymousPhoto(input: $input) {
+        id,
+        liked,
+        likes
+      }
+    }
+`;
 
 export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
     const { element, show } = useLazyLoad();
     const [liked, setLiked] = useLocalStorage(`like-${id}`, false);
-    const Icon = liked ? MdFavorite : MdFavoriteBorder;
+    const [mutation] = useMutation(LIKE_PHOTO);
 
+    const handleFavClick = () => {
+        if (!liked) { mutation({ variables: { input: { id } } }); }
+        setLiked(liked);
+    };
+    
     return (
         <Articule ref={element}>
             { show &&
@@ -23,10 +39,11 @@ export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
                             />
                         </ImgWrapper>
                      </a>
-                    <Button onClick={setLiked(!liked)}>
-                        <Icon size='32px'/>
-                        {likes} likes!
-                    </Button>
+                    <FavButton
+                        liked={liked}
+                        likes={likes}
+                        onClick={handleFavClick}
+                    />
                 </>
             }
         </Articule>
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { ListOfCategories } from "./ListOfCategories";
 export { PhotoCard } from "./PhotoCard";
 export { ListOfPhotoCards } from "./ListOfPhotoCards";
 export { Logo } from "./Logo";
+export { FavButton } from "./FavButton";
