acfd19772ccd1e62a85a2b2fb4624a7bd8c085d0
rolando22 <rolando2207@gmail.com>
2022-08-30 20:26:36 -0300

    [ADD] React.lazy en las pages de App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Logo, NavBar } from "./components/";
-import { Home, Detail, Favs, User, NotRegisteredUser, NotFound } from "./pages/";
 import { useAppContext } from "./hooks";
 import { GlobalStyles } from "./styles/GlobalStyles";
+
+const Detail = React.lazy(() => import ("./pages/")
+    .then(module => ({ default: module.Detail })));
+const Favs = React.lazy(() => import("./pages/")
+    .then(module => ({ default: module.Favs })));
+const Home = React.lazy(() => import("./pages/")
+    .then(module => ({ default: module.Home })));
+const NotFound = React.lazy(() => import("./pages/")
+    .then(module => ({ default: module.NotFound })));
+const NotRegisteredUser = React.lazy(() => import("./pages/")
+    .then(module => ({ default: module.NotRegisteredUser })));
+const User = React.lazy(() => import("./pages/")
+    .then(module => ({ default: module.User })));
 
 export function App () {
     const { isAuth } = useAppContext();
 
     return (
-        <BrowserRouter>
-            <GlobalStyles />
-            <Logo />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category/:id" element={<Home />} />
-                <Route path="/detail/:detailId" element={<Detail />} />
-                <Route path="/login" element={!isAuth ? <NotRegisteredUser /> : <Navigate replace to="/" />} />
-                <Route path="/favs" element={isAuth ? <Favs /> : <Navigate replace to="/login" />} />
-                <Route path="/user" element={isAuth ? <User /> : <Navigate replace to="/login" />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <NavBar />
-        </BrowserRouter>
+        <Suspense fallback={<div />}>
+            <BrowserRouter>
+                <GlobalStyles />
+                <Logo />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/category/:id" element={<Home />} />
+                    <Route path="/detail/:detailId" element={<Detail />} />
+                    <Route path="/login" element={!isAuth ? <NotRegisteredUser /> : <Navigate replace to="/" />} />
+                    <Route path="/favs" element={isAuth ? <Favs /> : <Navigate replace to="/login" />} />
+                    <Route path="/user" element={isAuth ? <User /> : <Navigate replace to="/login" />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+                <NavBar />
+            </BrowserRouter>
+        </Suspense>
     );
 };
